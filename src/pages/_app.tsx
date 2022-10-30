69339b9bf8dfc4b44d6295537159c33169a206b9
Long Ji-An <77440060+LongJiAn99@users.noreply.github.com>
2022-10-30 14:44:53 +0800

    First authentication initialisation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app';
 import NavBar from '../components/Layout/NavBar';
 import '../styles/index.css';
+import { SessionProvider } from "next-auth/react";
+import { Session } from 'next-auth';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps<{
+  session: Session;
+}>) {
   return (
     <div>
+      <SessionProvider session={pageProps.session}>
       <NavBar />
       <Component {...pageProps} />
+      </SessionProvider>
     </div>
   );
 }
