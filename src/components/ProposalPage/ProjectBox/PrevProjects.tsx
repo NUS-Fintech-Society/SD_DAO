78dea1caa2065e92def331197a5b94b65cb75f10
Hai Jin <99955221+coidng21@users.noreply.github.com>
2023-01-13 20:30:24 +0800

    Proposal Page

---
+++ src/components/ProposalPage/ProjectBox/PrevProjects.tsx
@@ -0,0 +1,28 @@
+import ProjectBox from "./ProjectBox"
+
+const PrevProject = () => {
+    return (
+        <>
+            <ProjectBox
+                title="Project 01"
+                aboutUrl="#none"
+                viewUrl="#none"
+                addUrl="#none"
+                />
+            <ProjectBox
+                title="Project 02"
+                aboutUrl="#none"
+                viewUrl="#none"
+                addUrl="#none"
+            />
+            <ProjectBox
+                title="Project 03"
+                aboutUrl="#none"
+                viewUrl="#none"
+                addUrl="#none"
+            />
+        </>
+    )
+}
+
+export default PrevProject;
