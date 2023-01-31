886faf2c7c2f40e26a25bc5e5e13d98f345be80b
Hai Jin <99955221+coidng21@users.noreply.github.com>
2023-01-31 21:05:15 +0800

    first commit

---
+++ src/pages/projects/createProject.tsx
@@ -0,0 +1,11 @@
+import CreateProjectPage from "../components/CreateProject/CreateProjectPage";
+
+export default function createProposal() {
+    return (
+        <>
+            <CreateProjectPage/>
+        </>
+    )
+}
+
+
