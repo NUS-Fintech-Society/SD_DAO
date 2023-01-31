8525f9bcc1834cf1b011f39d8ab8b265a0116a5b
Jovan <jovantan630@gmail.com>
2023-02-01 00:52:34 +0800

    Adjusted imports and file structure

---
+++ src/pages/projects/proposals/index.tsx
@@ -0,0 +1,10 @@
+import CreateProposalPage from "../../../components/CreateProposal/CreateProposalPage";
+
+//TODO: Shift this to /projects/proposals/createproposal after main proposal page is done
+export default function createProposal() {
+    return (
+        <>
+            <CreateProposalPage/>
+        </>
+    )
+}
