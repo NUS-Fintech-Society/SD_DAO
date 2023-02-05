d494a21e117672aa8cbefd616170eeeb490be363
loyhongshenggg <e0725346@u.nus.edu>
2023-02-06 00:15:51 +0800

    propsalpage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,20 @@ import NavBar from '../components/Layout/NavBar';
 import '../styles/index.css';
 import { SessionProvider } from "next-auth/react";
 import { Session } from 'next-auth';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App({ Component, pageProps }: AppProps<{
   session: Session;
 }>) {
   return (
-    <div className='bg-landing-page h-screen'>
+    <ChakraProvider>
+     <div className='bg-landing-page h-screen'> 
       <SessionProvider session={pageProps.session}>
       <NavBar />
       <Component {...pageProps} />
       </SessionProvider>
-    </div>
+     </div>
+    </ChakraProvider>
   );
 }
 
