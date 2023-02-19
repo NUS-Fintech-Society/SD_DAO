9adb16a511ec45b3f444d946d677c57829e4c882
felibunnyy <felibunbun13@gmail.com>
2023-02-20 02:05:52 +0800

    overwrite navbar

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -45,77 +45,79 @@ export default function SignInPage() {
 
   return (
     <>
-      <div className="py-5">
-        <form className="form mx-10 my-20 rounded">
-          <div className="grid grid-cols-2">
-            <div className="px-20 py-20">
-              <h1 className="text-3xl mb-5 mt-0"> Login </h1>
-              <div className="text-l">
-                <h2 className="mb-1"> Email </h2>
-                <div className="flex flex-col">
-                  <Input
-                    bg="white"
-                    variant="outline"
-                    className="mb-2 pt-2 pb-2 pl-4 rounded-full bg-[#C7C7C7]"
-                    type="email"
-                    name="Username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
+      <div className=" bg-login-page bg-no-repeat bg-cover bg-left-top h-screen -mt-16">
+        <div className="py-5">
+          <form className="form mx-10 my-20 rounded">
+            <div className="grid grid-cols-2">
+              <div className="px-20 py-20">
+                <h1 className="text-3xl mb-5 mt-0"> Login </h1>
+                <div className="text-l">
+                  <h2 className="mb-1"> Email </h2>
+                  <div className="flex flex-col">
+                    <Input
+                      bg="white"
+                      variant="outline"
+                      className="mb-2 pt-2 pb-2 pl-4 rounded-full bg-[#C7C7C7]"
+                      type="email"
+                      name="Username"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                  <h2 className="mb-1"> Password </h2>
+                  <div className="flex flex-row">
+                    <Input
+                      bg="white"
+                      className="pt-2 pb-2 pl-4 mr-5 w-full rounded-full bg-[#C7C7C7]"
+                      type={show ? "text" : "password"}
+                      name="Password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                      onClick={handleClick}
+                      className="pr-5 pt-2 pb-2 text-black"
+                    >
+                      {show ? "Hide" : "Show"}
+                    </Button>
+                  </div>
+                  <div className="flex flex-row justify-end mt-3">
+                    <Link href="/ResetPasswordEmail">Forgot password?</Link>
+                  </div>
                 </div>
-                <h2 className="mb-1"> Password </h2>
-                <div className="flex flex-row">
-                  <Input
-                    bg="white"
-                    className="pt-2 pb-2 pl-4 mr-5 w-full rounded-full bg-[#C7C7C7]"
-                    type={show ? "text" : "password"}
-                    name="Password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <Button
-                    onClick={handleClick}
-                    className="pr-5 pt-2 pb-2 text-black"
+                <div className="flex flex-col items-center justify-center">
+                  <button
+                    className="bg-fintech-yellow text-black font-bold rounded-2xl px-7 py-1.5 mt-10 font-chakraPetch tracking-widest hover:bg-yellow-400"
+                    onClick={handleLogin}
                   >
-                    {show ? "Hide" : "Show"}
-                  </Button>
-                </div>
-                <div className="flex flex-row justify-end mt-3">
-                  <Link href="/ResetPasswordEmail">Forgot password?</Link>
+                    LOGIN
+                  </button>
+                  <Link href="/signup">
+                    <div className="flex flex-col mt-2">
+                      <button
+                        className="text-black font-chakraPetch underline"
+                        /*onClick={() => setLoginState(!login)}*/
+                      >
+                        Sign Up
+                      </button>
+                    </div>
+                  </Link>
                 </div>
               </div>
-              <div className="flex flex-col items-center justify-center">
-                <button
-                  className="bg-fintech-yellow text-black font-bold rounded-2xl px-7 py-1.5 mt-10 font-chakraPetch tracking-widest hover:bg-yellow-400"
-                  onClick={handleLogin}
-                >
-                  LOGIN
-                </button>
-                <Link href="/signup">
-                  <div className="flex flex-col mt-2">
-                    <button
-                      className="text-black font-chakraPetch underline"
-                      /*onClick={() => setLoginState(!login)}*/
-                    >
-                      Sign Up
-                    </button>
-                  </div>
+              <div className=" flex flex-cols place-items-center">
+                <Link href="/">
+                  <img
+                    src="/fintech_logo.png"
+                    alt="Fintech Logo"
+                    className="cursor-pointer w-2/3 max-w-md mx-auto"
+                  />
                 </Link>
               </div>
             </div>
-            <div className=" flex flex-cols place-items-center">
-              <Link href="/">
-                <img
-                  src="/fintech_logo.png"
-                  alt="Fintech Logo"
-                  className="cursor-pointer w-2/3 max-w-md mx-auto"
-                />
-              </Link>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </>
   );
