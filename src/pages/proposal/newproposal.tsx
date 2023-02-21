08ee19c2e52cb0db4f5915d24c8728e54979514d
Hai Jin <99955221+coidng21@users.noreply.github.com>
2023-02-21 23:18:09 +0800

    Update newproposal.tsx

--- src/pages/proposal/newproposal.tsx
+++ src/pages/proposal/newproposal.tsx
@@ -1,1 +1,12 @@
+import CreateProjectPage from "../../components/CreateProject/CreateProjectPage";
+
+
+export default function createProposal() {
+    return (
+        <>
+            <CreateProjectPage/>
+        </>
+    )
+}
+
 
