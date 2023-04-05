917a88b1370abe3e6be3a707309ac96ca29a4e06
HAO ZEYU <823549721@qq.com>
2023-04-05 22:25:36 +0800

    change nextauth and download package dcryptjs

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,81 +6,87 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from "../../../server/db/client"
 import { loginSchema } from "./auth";
-import { verify } from "argon2";
+import { compare } from 'bcryptjs'
+import type { User } from '@prisma/client'
 
 export default NextAuth({
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt',
+        maxAge: 24 * 60 * 60,
     },
     adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            type: 'credentials',
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                email: { label: "Email", type: "text ", placeholder: "jsmith@gmail.com" },
-                password: { label: "Password", type: "password" },
-            },
-           authorize: async(credentials, req) => {
-                const creds = await loginSchema.parseAsync(credentials);
-                const user = await prisma.user.findFirst({
-                    where: {email: creds.email},
-                  });
-          
-                  if (!user) {
-                    return null;
-                  }
-          
-                  const isValidPassword = await verify(user.password, creds.password);
-          
-                  if (!isValidPassword) {
-                    return null;
-                  }
-                  return {
-                    id: user.id,
-                    email: user.email,
-                    username: user.username,
-                  };
-          
-                // Add logic here to look up the user from the credentials supplied
-                /*const res = await fetch("/your/endpoint", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
-                })
-                
-                const user = await res.json();
-                // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user
-                }
-                */
-             
+          name: 'NUS Email',
+          credentials: {},
+          authorize: async (credential) => {
+            try {
+              // Step 1: Destructure and get the email and password
+              const { email, password } = credential as {
+                email: string
+                password: string
+              }
+    
+              // Step 2: If no credentials are provided, throw an error
+              if (!credential || !email || !password) {
+                throw new Error('No email or password provided')
+              }
+    
+              // Step 3: Get the user by the email
+              const adapterUser = await prisma.user.findUnique({
+                where: { email },
+              })
+              if (!adapterUser) throw new Error('Invalid email or password')
+    
+              // Step 4: Type cast it to the type of User
+              const account = adapterUser as User
+    
+              // If the account is found, challenge the hashPassword with the password
+              const success = await compare(password, account.hashedPassword)
+              if (!success) throw new Error('Invalid email or password')
+    
+              // The user object is passed to the session callback in session.data.user
+              return {
+                id: account.id,
+                name: account.name,
+                email: account.email,
+                isAdmin: account.isAdmin,
+              }
+            } catch (e) {
+              throw new Error((e as Error).message)
             }
+          },
         }),
-    ],
+      ],
     callbacks: {
-        jwt: async({ token, account }) => {
-          // Persist the OAuth access_token to the token right after signin
-          if (account) {
-            token.accessToken = account.access_token
-            token.id = account.id
+        // We need this to ensure that the client knows when to log in
+        async session({ session, token }) {
+          if (session && session.user && token) {
+            session.user.id = token.sub || ''
+            session.user.image = token.picture
+          }
+    
+          if (token) {
+            session.isAdmin = token.isAdmin as boolean
+          }
+          return session
+        },
+    
+        async jwt({ token, user }) {
+          if (user) {
+            token.isAdmin = (user as User).isAdmin as boolean
+            token.picture = user.image
           }
           return token
-        }
-    },
+        },
+      },
 
-    // jwt: {
-    //     secret: "anything",
-    //     maxAge: 15 * 24 * 30 * 60, // 15 days
-    // },
-    // pages: {
-    //     signIn: "/pages/login",
-    //     newUser: "/pages/signup",
-    // },
+    jwt: {
+        secret: "",
+        maxAge: 24 * 60 * 60, 
+    },
+    pages: {
+        signIn: "/pages/login",
+        newUser: "/pages/signup",
+    },
 })
