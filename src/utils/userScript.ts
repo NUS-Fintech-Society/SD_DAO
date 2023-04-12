c073afa711e2b8e7ed6a2380a408f82896f1da31
HAO ZEYU <823549721@qq.com>
2023-04-12 15:59:20 +0800

    fix bugs

--- src/utils/userScript.ts
+++ src/utils/userScript.ts
@@ -3,27 +3,30 @@ const prisma = new PrismaClient()
 
 async function main() {
     await prisma.user.create({
-        data: {
-          attendance: 2,
-          batch: "22/23",
-          department: "Blockchain",
-          discord: "abc",
-          gender: "male",
-          faculty: "SOC",
-          hobbies: "badminton",
-          level: "1",
-          image: "https://images.unsplash.com/photo-1576245482660-6fcf7492b4e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          name: "Jon",
-          nus_email: "test@u.nus.edu",
-          personal_email: "test@hmail.com",
-          projects: "DAO",
-          roles: "Software Developer",
-          student_id: "1",
-          telegram: "@Jonthelegend",
-          total_events: "1",
-          wallet: "50",
-          year: "1"
-        }
+          data: {
+    //       attendance: 2,
+    //       batch: "22/23",
+    //       department: "Blockchain",
+    //       discord: "abc",
+    //       gender: "male",
+    //       faculty: "SOC",
+    //       hobbies: "badminton",
+    //       level: "1",
+    //       image: "https://images.unsplash.com/photo-1576245482660-6fcf7492b4e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+             name: "Jon",
+             email: "test@u.nus.edu",
+             hashedPassword: '123',
+             id: '123',
+             level:'1'
+    //       personal_email: "test@hmail.com",
+    //       projects: "DAO",
+    //       roles: "Software Developer",
+    //       student_id: "1",
+    //       telegram: "@Jonthelegend",
+    //       total_events: "1",
+    //       wallet: "50",
+    //       year: "1"
+       }
     })
 }
 
